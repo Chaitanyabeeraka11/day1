8891104d2fe5a94241bebc76762c69fd42c408c8
Chaitanyabeeraka11 <chaitanyabeeraka@gmail.com>
2024-06-24 14:51:57 +0530

    appbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import ButtonAppBar from "./Appbar";
 import Footer from "./Footer";
 import Header from "./Header";
 function App() {
   return (
     <div>
+      <ButtonAppBar />
       <Header />
       <Footer />
+      
     </div>
   );
 }
 
 export default App;
+
